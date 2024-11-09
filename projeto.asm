e4ed8151ad93c54bb2b8a506a04282eb3dd50e28
camilareixs <132021344+camilareixs@users.noreply.github.com>
2024-11-09 09:55:34 -0300

    Update projeto.asm

--- projeto.asm
+++ projeto.asm
@@ -1,47 +1,49 @@
 ORG 0000H
-    ; Inicio - começa com vermelho
-    MOV P2, #04H
-    CALL EXIBIR_MENSAGEM
+    ; Inicialização - Luz vermelha do carro e vermelho do pedestre
+    MOV P2, #14H            ;Liga LED vermelho carro (P2.2) e LED vermelho pedestre (P2.4)
 
 LOOP:
-    ; Luz vermelha por 5 segundos
-    CALL DELAY_5S
-    
-    ; Transição para o amarelo (avisar pedestre para aguardar)
-    MOV P2, #02H ; LED amarelo ligado (P2.1)
-    CALL EXIBIR_AGUARDE
+    ; Mantém a luz vermelha do carro por 3 segundos
     CALL DELAY_3S
     
-    ; Libera pedestres - muda para o verde
-    MOV P2, #01H ; LED verde ligado (P2.0)
-    CALL EXIBIR_PODE_PASSAR
-    CALL DELAY_10S
+    ; Transição para o amarelo (avisar pedestre para aguardar)
+    MOV P2, #12H            ;Liga LED amarelo carro (P2.1)
+    CALL DELAY_2S           ;Espera 2 segundos
+    
+    ; Libera pedestres - muda para o verde do carro e verde do pedestre
+    MOV P2, #09H            ;Liga LED verde carro (P2.0) e LED verde pedestre (P2.3)
+    CALL DELAY_8S           ;Espera 8 segundos para atravessar
 
-    ; Retorna para o vermelho e reinicia o loop
-    MOV P2, #04H  ; LED vermelho ligado (P2.2)
-    CALL EXIBIR_MENSAGEM
-    SJMP LOOP
-
-; Sub-rotinas de exibição
-EXIBIR_MENSAGEM:
-    ; Envia "Carro passando" ao LCD
-    ; terminar de desenvolver o código
-    RET
-EXIBIR_AGUARDE:
-    ; Envia "Aguarde" ao LCD
-    ; terminar de desenvolver o código
-    RET
-EXIBIR_PODE_PASSAR:
-    ; Envia "Pode atravessar" ao LCD
-    ; terminar de desenvolver o código
-    RET
+    ; Retorna para o vermelho do carro e acende o vermelho do pedestre
+    MOV P2, #14H            ;Liga LED vermelho carro (P2.2) e LED vermelho pedestre (P2.4)
+    SJMP LOOP               ;Reinicia o ciclo
 
 ; Rotinas de delay
-DELAY_5S:
-    RET
 DELAY_3S:
-    RET
-DELAY_10S:
+    ; Insere delay de 3 segundos
+    MOV R0, #30            ;Ajuste conforme necessário para seu clock
+DELAY_3S_LOOP:
+    NOP                     ;NOP para gerar delay
+    NOP
+    DJNZ R0, DELAY_3S_LOOP  ;Decrementa e repete
     RET
 
-END
+DELAY_2S:
+    ; Insere delay de 2 segundos
+    MOV R0, #20            ;Ajuste conforme necessário para seu clock
+DELAY_2S_LOOP:
+    NOP
+    NOP
+    DJNZ R0, DELAY_2S_LOOP  ;Decrementa e repete
+    RET
+
+DELAY_8S:
+    ; Insere delay de 8 segundos
+    MOV R0, #40            ;Ajuste conforme necessário para seu clock
+DELAY_8S_LOOP:
+    NOP
+    NOP
+    DJNZ R0, DELAY_8S_LOOP  ;Decrementa e repete
+    RET
+
+END 
